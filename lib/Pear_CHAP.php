cfea576545ddd801b6342f794f8fae2dab6654de
jandro5 <alexparalosamigos@gmail.com>
2020-07-15 09:54:34 +0200

    Update Pear_CHAP.php
    Fix ErrorException
    Array and string offset access syntax with curly braces is deprecated in php 7.4

--- lib/Pear_CHAP.php
+++ lib/Pear_CHAP.php
@@ -241,7 +241,7 @@ class Crypt_CHAP_MSv1 extends Crypt_CHAP
         $uni = '';
         $str = (string) $str;
         for ($i = 0; $i < strlen($str); $i++) {
-            $a = ord($str{$i}) << 8;
+            $a = ord($str[$i]) << 8;
             $uni .= sprintf("%X", $a);
         }
         return pack('H*', $uni);
@@ -413,7 +413,7 @@ class Crypt_CHAP_MSv1 extends Crypt_CHAP
 
         $bin = '';
         for ($i = 0; $i < strlen($key); $i++) {
-            $bin .= sprintf('%08s', decbin(ord($key{$i})));
+            $bin .= sprintf('%08s', decbin(ord($key[$i])));
         }
 
         $str1 = explode('-', substr(chunk_split($bin, 7, '-'), 0, -1));
